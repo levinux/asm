b2f30a5b5025e37a75fe36492b120b2c036d532b
levinux <levisan@gmail.com>
2020-01-29 17:45:19 -0700

    cambios a helloworld.asm para que funcionara en CRTC pero no :(

--- 6502/helloworld.asm
+++ 6502/helloworld.asm
@@ -2,22 +2,24 @@
 ; String printing limited to strings of 256 characters or less.
  
 a_cr	= $0d		; Carriage return.
-bsout	= $ffd2		; C64 KERNEL ROM, output a character to current device.
+bsout	= $9000		; C64 KERNEL ROM, output a character to current device.
 			; use $fded for Apple 2
-	.code
-        *=$c000
-	ldx #0		; Starting index 0 in X register.
+.code
+*=$c000
+ldx #0		; Starting index 0 in X register.
+
 printnext:
-	lda text,x	; Get character from string.
-	beq done	; If we read a 0 we're done.
-	jsr bsout	; Output character. 
-	inx		; Increment index to next character.
-	bne printnext	; Repeat if index doesn't overflow to 0.
+lda text,x	; Get character from string.
+beq done	; If we read a 0 we're done.
+jsr bsout	; Output character. 
+inx		; Increment index to next character.
+bne printnext	; Repeat if index doesn't overflow to 0.
+
 done:
-	rts		; Return from subroutine.
+rts		; Return from subroutine.
  
-	.rodata
+.rodata
  
 text:
-	.string  !pet "Hello world", a_cr,0
+.string  !pet "Hello world", a_cr,0
 
